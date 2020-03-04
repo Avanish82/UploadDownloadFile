7d5c32b287b47ae472b8ef3c1bc37756d6216afe
Avanish82 <avanish.as@gmail.com>
2020-03-04 10:47:02 +0530

    first commit

--- UploadDownload/src/test/java/com/uploadownload/multipartfile/SpringBootUploadownload.java
+++ UploadDownload/src/test/java/com/uploadownload/multipartfile/SpringBootUploadownload.java
@@ -1,4 +1,4 @@
-package com.javasampleapproach.multipartfile;
+package com.uploadownload.multipartfile;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootUploadMultipartFile2MySqlApplicationTests {
+public class SpringBootUploadownload {
 
 	@Test
 	public void contextLoads() {
